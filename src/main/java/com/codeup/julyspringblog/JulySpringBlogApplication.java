413b832faeb7329d3461c291fc5a35f6d913eb0e
Guillermo De Leon <guillermo.x.deleon@gmail.com>
2020-07-05 17:29:24 -0500

    first commit

---
+++ src/main/java/com/codeup/julyspringblog/JulySpringBlogApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.julyspringblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulySpringBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JulySpringBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/julyspringblog/JulySpringBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.julyspringblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JulySpringBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
