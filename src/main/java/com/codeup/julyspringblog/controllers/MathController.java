038e75c311a3b64256f9bd660cfdfb2a924d55e4
Guillermo De Leon <guillermo.x.deleon@gmail.com>
2020-07-05 22:01:48 -0500

    creating new controllers for spring blog

---
+++ src/main/java/com/codeup/julyspringblog/controllers/MathController.java
@@ -0,0 +1,17 @@
+package com.codeup.julyspringblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+
+    @GetMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public int add(@PathVariable int num1, @PathVariable int num2) {
+        return num1 + num2;
+    }
+}
