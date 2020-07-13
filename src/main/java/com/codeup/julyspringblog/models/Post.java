645685c56d5e125d6b5badfff4736429e5133b9a
Guillermo De Leon <guillermo.x.deleon@gmail.com>
2020-07-12 22:29:44 -0500

    updated postcontroller and added index and show html files

---
+++ src/main/java/com/codeup/julyspringblog/models/Post.java
@@ -0,0 +1,37 @@
+package com.codeup.julyspringblog.models;
+
+public class Post {
+    private long id;
+    private String title;
+    private String body;
+
+    public Post(long id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public String getBody() {
+        return this.body;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
